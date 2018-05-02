1a07606ea187d89082b5f698cb9729d662167c18
Nathaniel <nathanielzeeman@gmail.com>
2018-05-02 10:38:04 -0700

    cleaned up mark's script, created new upgradeNetwork scripts

---
+++ scripts/upgradeNetwork.sh
@@ -0,0 +1,7 @@
+echo 'Enter version number'
+read VERSION_NUMBER
+composer archive create -t dir -n .
+composer network install --card PeerAdmin@hlfv1 --archiveFile bond-marketplace@${VERSION_NUMBER}.bna
+composer network upgrade -c PeerAdmin@hlfv1 -n bond-marketplace -V $VERSION_NUMBER
+read -n 1 -s -r -p "Press any key to continue"
+echo
